1c9497c4d6b13fb493e4aa09d61e45e3bc8f7bbd
V0ula <p.dinaki@ihu.edu.gr>
2018-09-23 15:25:06 +0300

    Random Password Generator

--- RandomPassGenerator.sh
+++ RandomPassGenerator.sh
@@ -7,7 +7,7 @@ read LENGTH
 echo "Please select between hex or base64 according to how strong you wish your password to be"
 read ENCODING
 
-echo "Finally select the amount of the password that you would like to be generted. If you only wish to generate one password, please let empty this field"
+echo "Finally select the amount of passwords that you would like to be generted. If you wish to generate only one single password, please let this field empty"
 read AMOUNT
 
 for i in $(seq 1 $AMOUNT); do
